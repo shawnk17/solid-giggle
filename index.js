3cf02b166f28673d855d3b4aa16e95bf64ae3538
shawnd <axe2781@gmail.com>
2017-10-24 12:12:05 -0400

    t

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// var whotogreet ="Shawn ";
+var whotogreet ="Shawn ";
 
-// for(var i = 0; i < 10; i+ 1) (
-// console.log(i + "Hello, " + whotogreet  +"1"));
+for(var i = 0; i < 10; i+ 1) (
+console.log(i + "Hello, " + whotogreet  +"1"));
 
 
 )
